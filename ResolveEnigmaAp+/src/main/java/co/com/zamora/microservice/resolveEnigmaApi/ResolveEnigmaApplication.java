80e758d04e6e49712853d5f0a7cf7f8e23281775
Usuario <brayanzamora0612@gmail.com>
2024-02-27 22:57:25 -0500

    aplicacion con swagger

---
+++ ResolveEnigmaAp+/src/main/java/co/com/zamora/microservice/resolveEnigmaApi/ResolveEnigmaApplication.java
@@ -0,0 +1,16 @@
+package co.com.zamora.microservice.resolveEnigmaApi;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ResolveEnigmaApplication {
+
+ 
+    public static void main(String[] args) throws Exception {
+        new SpringApplication(ResolveEnigmaApplication.class).run(args);
+    }
+
+}
---
+++ ResolveEnigmaAp+/src/main/java/co/com/zamora/microservice/resolveEnigmaApi/api/ApiException.java
@@ -0,0 +1,10 @@
+package co.com.zamora.microservice.resolveEnigmaApi.api;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-02-27T20:18:15.561190100-05:00[America/Bogota]")
+public class ApiException extends Exception{
+    private int code;
+    public ApiException (int code, String msg) {
+        super(msg);
+        this.code = code;
+    }
+}
