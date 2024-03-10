db9cab70a88733ce403a65bf55fbe639400449c7
Usuario <brayanzamora0612@gmail.com>
2024-03-10 18:17:52 -0500

    Parcial primera parte

---
+++ ResolveEnigmaApiStep2/src/main/java/co/com/zamora/microservice/resolveEnigmaApi/ResolveEnigmaApplication.java
@@ -0,0 +1,21 @@
+package co.com.zamora.microservice.resolveEnigmaApi;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeGenerator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackages = { "co.com.zamora.microservice.resolveEnigmaApi", "co.com.zamora.microservice.resolveEnigmaApi.api" , "co.com.zamora.microservice.resolveEnigmaApi.config"})
+public class ResolveEnigmaApplication{
+
+    public static void main(String[] args) throws Exception {
+        new SpringApplication(ResolveEnigmaApplication.class).run(args);
+    }
+
+   
+}
---
+++ ResolveEnigmaApiStep2/src/main/java/co/com/zamora/microservice/resolveEnigmaApi/api/ApiException.java
@@ -0,0 +1,10 @@
+package co.com.zamora.microservice.resolveEnigmaApi.api;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-03-10T17:29:26.910-05:00[America/Bogota]")
+public class ApiException extends Exception{
+    private int code;
+    public ApiException (int code, String msg) {
+        super(msg);
+        this.code = code;
+    }
+}
